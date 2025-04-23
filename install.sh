5988af1da92b6bc596de6aa0ba3c6c280ff950c0
losky <271923379@qq.com>
2025-04-23 10:45:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -337,7 +337,7 @@ fetch_and_process_json() {
     local target_ip="$2"
     local json_data="$3"
     local parse_mode="$4"
-    local api_url="http://${target_ip}:880/api/setup"
+    local api_url="http://${target_ip}/api/setup"
 
     echo -e "\n\033[36m$title\033[0m"
     # 发送请求并处理响应
@@ -439,7 +439,7 @@ fetch_and_process_json() {
  
 echo -e "\n\033[36m检测PMail服务是否正常...\033[0m"
 
-ping_pmail_service "http://$PMAIL_IP:880/"
+ping_pmail_service "http://$PMAIL_IP/"
 
 
 
